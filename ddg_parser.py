f034c508a8fffc53b8ef5489f976b5c0b6e65ef8
Kevin Liu <kevin@potatofrom.space>
2017-04-15 12:05:40 -0400

    Get proper links in ddg_parser

--- ddg_parser.py
+++ ddg_parser.py
@@ -2,10 +2,10 @@ import requests
 from lxml import html
 
 uquery = input("Search Query: ")
-r = requests.get("https://duckduckgo.com/html/?q=" + uquery)
+r = requests.get("https://duckduckgo.com/html/?q=" + uquery, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
 
 tree = html.fromstring(r.content)
 
-links = tree.xpath('//a[@class="result__url"]/@href/text()')
+links = tree.xpath('//h2[@class="result__title"]/a[@class="result__a"]/@href')
 
 print("Links: ", links)
