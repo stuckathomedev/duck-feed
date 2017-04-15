3e3c1accafba829f404445a224efb050e6fb8a32
Kevin Liu <kevin@potatofrom.space>
2017-04-15 11:17:10 -0400

    Add keyboard interrupt fix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import signal
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -14,9 +16,15 @@ class Handler:
     def on_delete_window(*args):
         Gtk.main_quit(*args)
 
+    def on_menu_pressed(*args):
+        menu = builder.get_object('menu')
+        print("menu pressed")
+        menu.show_all()
+
     def on_search_btn_pressed(self, button):
         ddg_query = self.ddg_query_box.get_text()
-        self.print(ddg_query)
+        if not ddg_query == "":
+            self.print("Query: " + ddg_query)
 
 builder = Gtk.Builder()
 builder.add_from_file('window.glade')
@@ -25,4 +33,5 @@ builder.connect_signals(Handler())
 window = builder.get_object('window')
 window.show_all()
 
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 Gtk.main()
