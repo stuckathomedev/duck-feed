f2b08f51000ddb6c910a142d9e8819e0970ff0b5
kiwkii <aneeshedara@gmail.com>
2017-04-15 11:37:03 -0400

    Added LMXL library instead of html.parser

--- ddg_parser.py
+++ ddg_parser.py
@@ -1,62 +1,11 @@
 import requests
-from html.parser import HTMLParser
-from html.entities import name2codepoint
+from lxml import html
 
+uquery = input("Search Query: ")
+r = requests.get("https://duckduckgo.com/html/?q=" + uquery)
 
-# uquery stands for User Query
-def uquery():
-    # Gets user search query
-    uquery = input("Search Query: ")
+tree = html.fromstring(r.content)
 
-    # Submits query to Duck Duck Go
-    r = requests.get("https://duckduckgo.com/?q=" + uquery)
+links = tree.xpath('//a[@class="result__url"]/@href/text()')
 
-    # Returns scraped data for DDG Parser
-    scraped_data = r.text
-
-    texcord = open("log.txt", "w+")
-    texcord.write(scraped_data)
-    texcord.close()
-
-    return scraped_data
-
-# DuckDuckGo HTML Parser
-class ddg_parser(HTMLParser):
-    def handle_startendtag(self, tag, attrs):
-        print("stag: ", tag)
-        for attr in attrs:
-            print("attr: ", attr)
-
-    def handle_endtag(self, tag):
-        print("etag: ", tag)
-
-    def handle_data(self, data):
-        print("data: ", data)
-
-    def handle_comment(self, data):
-        print("comment: ", data)
-
-    def handle_entityref(self, name):
-        c = chr(name2codepoint[name])
-        print("nnet: ", c)
-
-    def handle_charref(self, name):
-        if name.startswith('x'):
-            c = chr(int(name[1:], 16))
-        else:
-            c = chr(int(name))
-        print("nu ent: ", c)
-
-    def handle_decl(self, data):
-        print("Decl: ", data)
-
-parser = ddg_parser()
-
-
-def parser_feed(data):
-    feed_export = parser.feed(data)
-    return feed_export
-
-scraped_data = uquery()
-parser_feed(scraped_data)
-
+print("Links: ", links)
